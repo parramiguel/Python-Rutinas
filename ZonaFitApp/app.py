0edb929a0ca80103a908e297074e100c5d447995
parramiguel <ingparrasojo@gmail.com>
2024-02-28 17:03:16 -0500

    miércoles 28/02/2024

---
+++ ZonaFitApp/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/') # url: http://localhost:5000/
+@app.route('/index.html') # url: http://localhost:5000/index.html
+def inicio():
+    app.logger.debug('Entramos al path de inicio/')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
