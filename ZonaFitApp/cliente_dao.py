09699831c2c876d13c58e680ad9b19fc103ace37
parramiguel <ingparrasojo@gmail.com>
2024-03-08 17:33:36 -0500

    viernes 08/03/2024

--- ZonaFitApp/cliente_dao.py
+++ ZonaFitApp/cliente_dao.py
@@ -4,6 +4,7 @@ from conexion import Conexion
 
 class ClienteDAO:
     SELECCIONAR = 'SELECT * FROM cliente'
+    SELECCIONAR_ID = 'SELECT * FROM cliente WHERE id=%s'
     INSERTAR = 'INSERT INTO cliente(nombre, apellido, membresia) VALUES(%s, %s, %s)'
     ACTUALIZAR = 'UPDATE cliente SET nombre=%s, apellido=%s, membresia=%s WHERE id=%s'
     ELIMINAR = 'DELETE FROM cliente WHERE id=%s'
@@ -29,6 +30,27 @@ class ClienteDAO:
             if conexion is not None:
                 cursor.close()
                 Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def seleccionar_por_id(cls,id):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (id,)
+            cursor.execute(cls.SELECCIONAR_ID,valores)
+            registro = cursor.fetchone()
+            # Mapeo clase-tabla cliente
+            cliente = Cliente(registro[0], registro[1],
+                              registro[2], registro[3])
+            return cliente
+        except Exception as e:
+            print(f'Excepción al seleccionar cliente por id {e}')
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
     @classmethod
     def insertar(cls,cliente):
         conexion = None
